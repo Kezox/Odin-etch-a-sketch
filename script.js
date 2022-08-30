83b85c5d1eebc9ffa0c694f27ba797dcd9a0bfc1
Ilija <ilija.kezovic4869@gmail.com>
2022-08-31 00:34:46 +0200

    Add event listener for hovering over grid items

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const container = document.querySelector('.container');
+const grid = document.createElement('div');
 
 for (let i = 0;i < 256; i++) {
-    const grid = document.createElement('div');
+const grid = document.createElement('div');   
 grid.classList.add('square');
 container.appendChild(grid);
-
 }
+
+
+const squaresAll = document.querySelectorAll('.square');
+squaresAll.forEach((square) => {
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = 'black';
+    
+    })
+    
+})
 
